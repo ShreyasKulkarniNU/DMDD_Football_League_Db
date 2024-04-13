aec27b0244c6cdc695a46073452c4bd82982756b
Balasubramanian U <ubalasubramanian03@gmail.com>
2024-04-12 21:00:05 -0400

    DDL, DML - minor correction

---
+++ reports.sql
@@ -0,0 +1,7 @@
+SELECT * FROM ticket_sales_match_wise;
+SELECT * FROM match_summary_view;
+SELECT * FROM revenue_of_the_club_through_merchandise;
+SELECT * FROM player_PerformanceTrack_Attacker_Individual_View;
+SELECT * FROM Merchandise_Items_View;
+SELECT * FROM MerchandisePriceRange;
+
